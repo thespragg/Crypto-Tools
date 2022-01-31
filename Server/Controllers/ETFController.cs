1d4dc7c7fba6d73a41b1f7e4a8832e4836085642
Alistair Spragg <alistair.spragg@gmail.com>
2022-01-31 22:12:52 +0000

    Implement ETF controller

--- Server/Controllers/ETFController.cs
+++ Server/Controllers/ETFController.cs
@@ -1,15 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Globalization;
+using Crypto_Tools.DAL;
+using Crypto_Tools.PortfolioStrategies;
+using Microsoft.AspNetCore.Mvc;
+using static Crypto_Tools.PortfolioStrategies.TopCoinsETF;
 
-namespace Crypto_Tools.Controllers
+namespace Crypto_Tools.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ETFController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ETFController : ControllerBase
-    {
-        [HttpGet]
-        public IActionResult Get()
-        {
+    private readonly ICoinPriceService _priceService;
+    private readonly IMarketCapService _mcapService;
+    public ETFController(IMarketCapService mcapService, ICoinPriceService coinPriceService) => (_mcapService, _priceService) = (mcapService, coinPriceService);
 
+
+    [HttpGet]
+    public async Task<IActionResult> Get(int amnt, int coins, string interval, string start, string end)
+    {
+        if (coins > 500) return BadRequest();
+        if (!Enum.TryParse(typeof(DcaInterval), interval, out var parsedInterval)) return BadRequest();
+        try
+        {
+            var startDate = DateTime.ParseExact(start, "yyyyMMdd", CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact(end, "yyyyMMdd", CultureInfo.InvariantCulture);
+            var etf = new TopCoinsETF(_mcapService, _priceService);
+            return Ok(await etf.Run(amnt, coins, (DcaInterval)parsedInterval!, startDate, endDate));
         }
+        catch(Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+
     }
 }
+
