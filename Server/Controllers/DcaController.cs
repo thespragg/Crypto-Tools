4876873121cc4fe2dd59c66dac7d03cf981751a1
Alistair Spragg <alistair.spragg@gmail.com>
2022-02-01 18:58:54 +0000

    Add generic dca strategy

---
+++ Server/Controllers/DcaController.cs
@@ -0,0 +1,37 @@
+﻿using Crypto_Tools.DAL;
+using Crypto_Tools.Models;
+using Crypto_Tools.PortfolioStrategies;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Text.Json;
+
+namespace Crypto_Tools.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DcaController : ControllerBase
+    {
+        private readonly ICoinPriceService _priceService;
+        public DcaController(ICoinPriceService coinPriceService) => (_priceService) = (coinPriceService);
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string allocation, int amnt, string interval, string start, string end)
+        {
+            var allocations = JsonSerializer.Deserialize<List<CoinAllocation>>(allocation, new JsonSerializerOptions {  PropertyNameCaseInsensitive = true });
+            if (allocations == null) return BadRequest();
+            if (!Enum.TryParse(typeof(DcaInterval), interval, out var parsedInterval)) return BadRequest();
+            try
+            {
+                var startDate = DateTime.ParseExact(start, "yyyyMMdd", CultureInfo.InvariantCulture);
+                var endDate = DateTime.ParseExact(end, "yyyyMMdd", CultureInfo.InvariantCulture);
+                var dca = new GenericDCA(_priceService);
+                var portfolio = await dca.Run(allocations, amnt, (DcaInterval)parsedInterval!, startDate, endDate);
+                return Ok(portfolio);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Server/PortfolioStrategies/GenericDCA.cs
@@ -0,0 +1,96 @@
+﻿using Crypto_Tools.DAL;
+using Crypto_Tools.DAL.Models;
+using Crypto_Tools.Models;
+
+namespace Crypto_Tools.PortfolioStrategies;
+
+public class GenericDCA
+{
+    private readonly ICoinPriceService _priceService;
+    private Dictionary<string, List<TimestampedPrice>> _prices = new();
+    public GenericDCA(ICoinPriceService coinPriceService) => (_priceService) = (coinPriceService);
+    public async Task<SimulationResult?> Run(List<CoinAllocation> allocations, int amnt, DcaInterval interval, DateTime start, DateTime end)
+    {
+        var coins = allocations.Select(x => x.Name).Distinct().ToList();
+        _prices = await GetPrices(coins, start, end);
+
+        DateTime? period = start;
+        if (period == null) return null;
+        var periodCount = 0;
+
+        var portfolio = new Dictionary<string, float>();
+        var portfolioValue = new List<PortfolioSnapshot>();
+        var coinProfits = new Dictionary<string, CoinPriceHolder>();
+
+        foreach (var coin in coins)
+        {
+            coinProfits.Add(coin, new CoinPriceHolder());
+            portfolio.Add(coin, 0f);
+        }
+
+        while (period != null)
+        {
+            periodCount += 1;
+
+            var snapshot = new PortfolioSnapshot()
+            {
+                Date = (DateTime)period,
+                
+            };
+
+            var notSpent = 0f;
+            foreach(var coinAllocation in allocations)
+            {
+                var dcaAmnt = amnt * (coinAllocation.Allocation / 100);
+                var currPrice = GetPriceOnDate(coinAllocation.Name, (DateTime)period);
+                if (currPrice == null)
+                {
+                    notSpent += dcaAmnt;
+                    continue;
+                }
+                portfolio[coinAllocation.Name] += dcaAmnt / (float)currPrice;
+                coinProfits[coinAllocation.Name].Spent += dcaAmnt;
+                var value = portfolio[coinAllocation.Name] * (float)currPrice;
+                coinProfits[coinAllocation.Name].Value = value;
+                snapshot.Value += value;
+            }
+
+            snapshot.Spent = (periodCount * amnt) - notSpent;
+            portfolioValue.Add(snapshot);
+
+            period = interval switch
+            {
+                DcaInterval.weekly => period = period.Value.AddDays(7),
+                DcaInterval.monthly => period = period.Value.AddMonths(1),
+                _ => null
+            };
+            if (period > end) period = null;
+        }
+
+        return new SimulationResult(portfolioValue, coinProfits.Select(x => new CoinProfit(x.Key, x.Value.Value - x.Value.Spent)).ToList());
+    }
+
+    private float? GetPriceOnDate(string coin, DateTime date)
+    {
+        if (!_prices.ContainsKey(coin)) return null;
+        var dict = _prices[coin];
+        if (!dict.Any(x => x.Date.Date == date.Date)) return null;
+        return dict.FirstOrDefault(x => x.Date.Date == date.Date)!.Price;
+    }
+
+    private async Task<Dictionary<string, List<TimestampedPrice>>> GetPrices(IList<string> coins, DateTime start, DateTime end)
+    {
+        var res = new Dictionary<string, List<TimestampedPrice>>();
+        foreach (var coin in coins)
+        {
+            var storedCoin = await _priceService.Find(coin);
+            if (storedCoin == null) continue;
+            var prices = storedCoin!.Prices.Where(x => x.Date >= start && x.Date <= end).ToList();
+            if (prices.Count == 0) continue;
+            res.Add(coin, prices);
+        }
+        return res;
+    }
+}
+
+
