bc39dd625c07da2cec2e7924566473a5ac18d61f
Alistair Spragg <alistair.spragg@gmail.com>
2022-02-01 18:58:24 +0000

    Create coin controller

---
+++ Server/Controllers/CoinController.cs
@@ -0,0 +1,16 @@
+﻿using Crypto_Tools.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crypto_Tools.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoinController : ControllerBase
+    {
+        private readonly ICoinPriceService _coins;
+        public CoinController(ICoinPriceService coins) => (_coins) = (coins);
+
+        [HttpGet]
+        public async Task<IActionResult> Get() => Ok((await _coins.Get()).Select(x => x.Name));
+    }
+}
