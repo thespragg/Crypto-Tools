1dd434947563714714277fef471e70fc5992cb9f
Alistair Spragg <alistair.spragg@gmail.com>
2022-02-07 17:12:46 +0000

    Get ready for publish

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 namespace Crypto_Tools;
 
 public class Program
@@ -6,9 +8,23 @@ public class Program
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
+            .ConfigureHostConfiguration(hostConfig =>
+            {
+                hostConfig.SetBasePath(GetApplicationRoot());
+                hostConfig.AddEnvironmentVariables();
+                hostConfig.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).Build();
+            })
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
             });
+
+    public static string GetApplicationRoot()
+    {
+        var exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        if (exePath!.Contains("file:")) exePath = exePath.Remove(0, 5);
+        if (!exePath.StartsWith("/var")) exePath = AppDomain.CurrentDomain.BaseDirectory;
+        return exePath;
+    }
 }
 
